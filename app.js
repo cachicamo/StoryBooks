6742436ee1db6bf199a0e7a96f18e3cb1491dfad
Jesus Porrello <Jporrello@gmail.com>
2021-03-03 10:46:21 -0500

    initital commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+
+//Load config
+dotenv.config({ path: './config/config.env' })
+
+// Passport Config
+require('./config/passport')(passport)
+
+connectDB()
+
+const app = express()
+
+// Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+
+// Logging with morgan
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
+// Handlebars middleware
+app.engine('.hbs', exphbs({helpers: {
+    formatDate, stripTags, truncate, editIcon, select }, defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+   }))
+
+
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`) 
+)
---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+// @desc    Auth with Google
+// @route   GET /auth/google
+
+router.get('/google', passport.authenticate('google', { scope: ['profile']}))
+
+// @desc    Google Auth callback
+// @route   GET /auth/google/callback
+
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: '/'}), (req, res) => res.redirect('/dashboard')
+)
+
+// @desc    User Logout
+// @route   GET /auth/logout
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+module.exports = router
